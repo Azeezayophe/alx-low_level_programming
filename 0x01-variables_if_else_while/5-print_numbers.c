f541668f3a9393e343f5937202b9c7c2528af389
Azeezayophe <azeezayophe01@gmail.com>
2022-06-07 04:52:26 +0100

    me

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<time.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int num;
+for (num = 48 ; num <= 57 ; num++)
+{
+putchar(num);
+}
+putchar('\n');
+return (0);
+}
