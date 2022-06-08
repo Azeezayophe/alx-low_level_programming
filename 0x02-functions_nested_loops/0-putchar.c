639fc59f91f3cc48ac3e5774d99f1f027fdc9847
Azeezayophe <azeezayophe01@gmail.com>
2022-06-08 12:19:03 +0100

    mine

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -6,7 +6,13 @@
  */
 int main(void)
 {
-_putchar("_putchar\n");
-
+int str[] = {45, 122, 177, 116, 99, 104, 97, 114};
+int count, sz;
+sz = sizeof(str) / sizeof(int);
+for (count = 0; count < sz; count++)
+{
+_putchar(str[count]);
+}
+_putchar('\n');
 return (0);
 }
