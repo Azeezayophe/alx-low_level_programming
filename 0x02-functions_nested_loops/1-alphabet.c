4aff735defdd7c3620efaddd3b214c4d5d8d2a40
Azeezayophe <azeezayophe01@gmail.com>
2022-06-08 13:00:30 +0100

    me

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,14 @@
+#include"main.h"
+/**
+ * print_alphabet - print alphabets in lowercase
+ *
+ * Return: Always 0 (Success)
+ */
+void print_alphabet(void);
+{
+char alp;
+for (alp = 97 ; alp <= 122 ; alp++)
+{
+_putchar(alp);
+}
+}
