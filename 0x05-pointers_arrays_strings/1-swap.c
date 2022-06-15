4969b8a2ce6a4877c2f0dc98cbeb881ee76d5eb9
Azeezayophe <azeezayophe01@gmail.com>
2022-06-15 23:36:47 +0100

    I love C

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,12 @@
+#include"main.h"
+/**
+* swap_int - swaps the values of two intigers
+* @a: integer 1
+* @b: integer 2
+**/
+void swap_int(int *a, int *b)
+{
+int c = *a;
+*a = *b;
+*b = c;
+}
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -3,6 +3,7 @@
 
 char *_strcat(char *dest, char *src); 
 void reset_to_98(int *n);
+void swap_int(int *a, int *b);
 char *_strncat(char *dest, char *src, int n);
 int _strcmp(char *s1, char *s2);
 void reverse_array(int *a, int n);
