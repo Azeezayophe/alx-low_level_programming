62afb6bdb5a3a01cffe336db0ed5b45aeb99094a
Azeezayophe <azeezayophe01@gmail.com>
2022-06-16 07:15:05 +0100

    mine

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -11,6 +11,7 @@ void rev_string(char *s);
 void puts2(char *str);
 void puts_half(char *str);
 void print_array(int *a, int n);
+char *_strcpy(char *dest, char *src);
 char *_strncat(char *dest, char *src, int n);
 int _strcmp(char *s1, char *s2);
 void reverse_array(int *a, int n);
