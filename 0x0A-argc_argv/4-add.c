97d054c16f747e0efba3f22ce660df93d3fa1b66
Azeezayophe <azeezayophe01@mail.com>
2022-06-24 20:46:32 -0700

    mine

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,26 +1,33 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <ctype.h>
-#include <string.h>
 /**
-* main -add positive numbers
-* @argc: int
-* @argv: char *
-* Return: always 0
+* main - function to print out the change that needs to given
+* @argc: number of arguments to be passed
+* @argv: number of variables to calculate
+* Return: always 0 for sucess
 */
-int main(int argc, char *argv[])
+int main(int argc, char **argv)
 {
-int i;
-int sum = 0;
-if (argc >= 1)
+int total, change;
+if (argc < 2)
 {
-for (i = 1; i <= (argc - 1); i++)
-{
-sum = sum + atoi(argv[i]);
+printf("Error\n");
+return (1);
 }
-printf("%d\n", sum);
+change = atoi(argv[1]);
+for (total = 0; change > 0; total++)
+{
+if (change - 25 >= 0)
+change = change - 25;
+else if (change - 10 >= 0)
+change = change - 10;
+else if (change - 5 >= 0)
+change = change - 5;
+else if (change - 2 >= 0)
+change = change - 2;
+else if (change - 1 >= 0)
+change = change - 1;
 }
-if (argc == 1)
-printf("0\n");
+printf("%d\n", total);
 return (0);
 }
