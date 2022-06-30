54002a8df1f5b20fe96c97341823bd71faf45ee2
Azeezayophe <azeezayophe01@gmail.com>
2022-06-30 07:17:36 +0100

    to_allocate_memory

--- 0x0B-malloc_free/3-main.c
+++ 0x0B-malloc_free/3-main.c
@@ -1,7 +1,6 @@
 #include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
-
 /**
 * print_grid - prints a grid of integers
 * @grid: the address of the two dimensional grid
@@ -13,38 +12,35 @@ void print_grid(int **grid, int width, int height)
 {
 int w;
 int h;
-
-		    h = 0;
-		        while (h < height)
-				    {
-					            w = 0;
-						            while (w < width)
-								            {
-										                printf("%d ", grid[h][w]);
-												            w++;
-													            }
-							            printf("\n");
-								            h++;
-									        }   
+h = 0;
+while (h < height)
+{
+w = 0;
+while (w < width)
+{
+printf("%d ", grid[h][w]);
+w++;
 }
-
+printf("\n");
+h++;
+}   
+}
 /**
 * main - check the code for ALX School students.
 * Return: Always 0.
 */
 int main(void)
 {
-	    int **grid;
-
-	        grid = alloc_grid(6, 4);
-		    if (grid == NULL)
-			        {
-					        return (1);
-						    }
-		        print_grid(grid, 6, 4);
-			    printf("\n");
-			        grid[0][3] = 98;
-				    grid[3][4] = 402;
-				        print_grid(grid, 6, 4);
-					    return (0);
+int **grid;
+grid = alloc_grid(6, 4);
+if (grid == NULL)
+{
+return (1);
+}
+print_grid(grid, 6, 4);
+printf("\n");
+grid[0][3] = 98;
+grid[3][4] = 402;
+print_grid(grid, 6, 4);
+return (0);
 }
