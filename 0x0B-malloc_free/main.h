3452898d9f884e5440ddcc6c4fc07e033bfb779f
Azeezayophe <azeezayophe01@gmail.com>
2022-06-30 07:44:20 +0100

    mine

--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -6,7 +6,7 @@ char *create_array(unsigned int size, char c);
 int **alloc_grid(int width, int height);
 char *_strdup(char *str);
 void free_grid(int **grid, int height);
-
+char *argstostr(int ac, char **av);
 
 
 
