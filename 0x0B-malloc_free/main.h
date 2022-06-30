4bf5e3dbb1e3178dba0a8c7f5e8a417b5b7070c4
Azeezayophe <azeezayophe01@gmail.com>
2022-06-30 07:19:52 +0100

    to_allocate_memory

--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -3,13 +3,8 @@
 
 int _putchar(char c);
 char *create_array(unsigned int size, char c);
-<<<<<<< HEAD
 int **alloc_grid(int width, int height);
-
-
-=======
 char *_strdup(char *str);
->>>>>>> 225880c632745aab98e31204bd5fa19b6bc7760e
 
 
 
