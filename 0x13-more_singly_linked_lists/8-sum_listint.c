fa2ead804086b8e492946c71441d982a3f77c313
Azeezayophe <azeezayophe01@gmail.com>
2022-07-19 01:23:10 +0100

    mine

---
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include "lists.h"
+/**
+* sum_listint - return sum of list data
+* @head: head node
+* Return: sum
+*/
+int sum_listint(listint_t *head)
+{
+int sum = 0;
+if (!head)
+return (sum);
+while (head)
+{
+sum += head->n;
+head = head->next;
+}
+return (sum);
+}
